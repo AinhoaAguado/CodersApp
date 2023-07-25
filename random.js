0b769d424814e71021b6bfdd020ef1885f415128
Jassed Martinez <jassedgmartinez@gmail.com>
2023-07-25 11:34:23 +0200

    animacion

--- random.js
+++ random.js
@@ -1,8 +1,6 @@
 // Creamos una variable global para almacenar los datos del servidor
 let arraydatos = [];
 
-
-
 // Función para obtener los datos del servidor y guardarlos en la variable global
 function obtenerDatosDelServidor() {
   const url = "https://page-backend-api.onrender.com/datos";
@@ -15,6 +13,9 @@ function obtenerDatosDelServidor() {
       // Una vez que los datos se han cargado, habilitamos el botón
       const button = document.getElementById("btn-seleccion");
       button.disabled = false;
+    })
+    .catch(error => {
+      console.error("Error al obtener los datos del servidor:", error);
     });
 }
 
@@ -26,7 +27,6 @@ const btn = document.getElementById("btn-seleccion");
 
 // Deshabilitamos el botón al inicio hasta que los datos se carguen
 btn.disabled = true;
-
 
 // Creamos una copia del array original de los coders
 let bufferImagenes = [];
@@ -43,38 +43,56 @@ function cargarMostrarImagenAleatoria() {
     return;
   }
 
-  // Obtenemos un índice aleatorio para seleccionar una imagen
-  let indiceImagenAleatoria = generarEnteroAleatorio(bufferImagenes.length);
+  // Deshabilitamos el botón mientras se selecciona y muestra el coder
+  btn.disabled = true;
 
-  // Obtenemos la imagen aleatoria según el índice obtenido
-  let imagenAleatoria = bufferImagenes[indiceImagenAleatoria];
+  // Agregamos un mensaje de suspenso o imagen de carga antes de mostrar al coder
+  const mensajeSuspenso = document.createElement("h2");
+  mensajeSuspenso.textContent = "Seleccionando coder aleatorio...";
+  document.body.appendChild(mensajeSuspenso);
 
-  // Eliminamos la imagen seleccionada del buffer para evitar que se repita
-  bufferImagenes.splice(indiceImagenAleatoria, 1);
+  // Establecemos un tiempo de espera (en milisegundos) antes de mostrar al coder seleccionado
+  const tiempoEspera = 2000; // 2 segundos (ajústalo según tus preferencias)
+  setTimeout(function() {
+    // Obtenemos un índice aleatorio para seleccionar una imagen
+    let indiceImagenAleatoria = generarEnteroAleatorio(bufferImagenes.length);
 
-  // Creamos un nuevo elemento de imagen en el DOM
-  let nuevoElementoImagen = document.createElement("img");
-  nuevoElementoImagen.src = imagenAleatoria.src;
-  nuevoElementoImagen.width = 100; // Tamaño predeterminado
-  nuevoElementoImagen.height = 100; // Tamaño predeterminado
-  nuevoElementoImagen.setAttribute("data-nombre", imagenAleatoria.nombre);
+    // Obtenemos la imagen aleatoria según el índice obtenido
+    let imagenAleatoria = bufferImagenes[indiceImagenAleatoria];
 
-  // Agregamos la imagen al documento HTML
-  document.body.appendChild(nuevoElementoImagen);
+    // Eliminamos la imagen seleccionada del buffer para evitar que se repita
+    bufferImagenes.splice(indiceImagenAleatoria, 1);
 
-  // Obtenemos el nombre de la imagen aleatoria
-  let nombreImagenAleatoria = imagenAleatoria.nombre;
+    // Creamos un nuevo elemento de imagen en el DOM
+    let nuevoElementoImagen = document.createElement("img");
+    nuevoElementoImagen.src = imagenAleatoria.src;
+    nuevoElementoImagen.width = 100; // Tamaño predeterminado
+    nuevoElementoImagen.height = 100; // Tamaño predeterminado
+    nuevoElementoImagen.setAttribute("data-nombre", imagenAleatoria.nombre);
 
-  // Creamos un nuevo elemento de párrafo en el DOM
-  let nuevoElementoNombre = document.createElement("p");
-  nuevoElementoNombre.textContent = nombreImagenAleatoria;
+    // Agregamos la imagen al documento HTML
+    document.body.appendChild(nuevoElementoImagen);
 
-  // Agregamos el nombre al documento HTML
-  document.body.appendChild(nuevoElementoNombre);
+    // Obtenemos el nombre de la imagen aleatoria
+    let nombreImagenAleatoria = imagenAleatoria.nombre;
+
+    // Creamos un nuevo elemento de párrafo en el DOM
+    let nuevoElementoNombre = document.createElement("p");
+    nuevoElementoNombre.textContent = nombreImagenAleatoria;
+
+    // Agregamos el nombre al documento HTML
+    document.body.appendChild(nuevoElementoNombre);
+
+    // Eliminamos el mensaje de suspenso o imagen de carga
+    document.body.removeChild(mensajeSuspenso);
+
+    // Habilitamos nuevamente el botón para obtener un nuevo coder
+    btn.disabled = false;
+  }, tiempoEspera);
 }
 
 // Agregamos un evento de clic al botón para ejecutar la función
-btn.addEventListener('click', cargarMostrarImagenAleatoria);
+btn.addEventListener("click", cargarMostrarImagenAleatoria);
 
 // Función para generar un número entero aleatorio dentro de un rango
 function generarEnteroAleatorio(cantidadImagenes) {
